67c7024c0f4dbcb8cc13b7582ce8a9992e7e859d
Kamaroncio <167287617+Kamaroncio@users.noreply.github.com>
2025-02-20 12:21:26 +0100

    gamerscript.bat

---
+++ gamerscript.bat
@@ -0,0 +1,52 @@
+:: WinScript 
+@echo off
+:: Check if the script is running as admin
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+    color 4
+    echo This script requires administrator privileges.
+    echo Please run WinScript as an administrator.
+    pause
+    exit
+)
+:: Admin privileges confirmed, continue execution
+setlocal EnableExtensions DisableDelayedExpansion
+echo -- Killing OneDrive Process
+taskkill /f /im OneDrive.exe
+echo -- Uninstalling OneDrive through the installers
+if exist "%SystemRoot%\System32\OneDriveSetup.exe" (
+    "%SystemRoot%\System32\OneDriveSetup.exe" /uninstall
+)
+if exist "%SystemRoot%\SysWOW64\OneDriveSetup.exe" (
+    "%SystemRoot%\SysWOW64\OneDriveSetup.exe" /uninstall
+)
+echo -- Removing OneDrive registry keys
+reg delete "HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+reg delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+echo -- Removing OneDrive folders
+rd "%UserProfile%\OneDrive" /Q /S
+rd "%LocalAppData%\Microsoft\OneDrive" /Q /S
+rd "%ProgramData%\Microsoft\OneDrive" /Q /S
+rd "C:\OneDriveTemp" /Q /S
+echo -- Disabling Mouse Acceleration
+reg add "HKCU\Control Panel\Mouse" /v "MouseSpeed" /t REG_SZ /d "0" /f
+reg add "HKCU\Control Panel\Mouse" /v "MouseThreshold1" /t REG_SZ /d "0" /f
+reg add "HKCU\Control Panel\Mouse" /v "MouseThreshold2" /t REG_SZ /d "0" /f
+echo -- Disabling Game Bar
+reg add "HKLM\SOFTWARE\Polices\Microsoft\Windows\GameDVR" /v "AllowGameDVR" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" /v "AppCaptureEnabled" /t REG_DWORD /d 0 /f
+reg add "HKCU\SOFTWARE\Microsoft\GameBar" /v "UseNexusForGameBarEnabled" /t REG_DWORD /d 0 /f
+reg add "HKCU\SOFTWARE\Microsoft\GameBar" /v "ShowStartupPanel" /t REG_DWORD /d 0 /f
+echo -- Enabling Dark Mode
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
+echo -- Showing File Extensions
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d 0 /f
+echo -- Disabling Sticky Keys
+reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v "Flags" /t REG_SZ /d "58" /f
+:: Pause the script
+pause
+:: Restore previous environment
+endlocal
+:: Exit the script
+taskkill /f /im explorer.exe & start explorer & exit /b 0
